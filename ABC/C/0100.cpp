f587b30f5d4e2391896fed3514d528e56c8cef7d
taro_masuda <taro.massu@gmail.com>
2020-04-18 00:17:49 +0900

    Added C_100.

---
+++ ABC/C/0100.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+void Main()
+{
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  int cnt = 0;
+  rep(i,N) {
+    cin >> a[i];
+    while (a[i] %2 == 0) {
+      cnt++;
+      a[i] /= 2;
+    }
+  }
+  cout << cnt << endl;
+}
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(20);
+  Main();
+  return 0;
+}
