bf89cff0f43797c06f87798f872f1f20fee85712
taro_masuda <taro.massu@gmail.com>
2020-05-08 22:10:14 +0900

    Added ABC/C/0070.cpp.

---
+++ ABC/C/0070.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+  if (a % gcd(a,b) == 0) {
+    ll agcd = a / gcd(a,b);
+    return b * agcd;
+  }
+  else if (b % gcd(a,b) == 0) {
+    ll bgcd = b / gcd(a,b);
+  	return a * bgcd;
+  }
+  return a*b / gcd(a,b);
+}
+
+void Main()
+{
+  ll N; cin >> N;
+  ll T;
+  ll ans = 1;
+  rep(i,N) {
+    cin >> T;
+    ans = lcm(T, ans);
+  }
+  cout << ans << endl;
+}
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(20);
+  Main();
+  return 0;
+}
