fbd0237f034ec94b87c97b3482d692c83b0eb2b9
taro_masuda <taro.massu@gmail.com>
2020-07-03 00:23:41 +0900

    Added ABC/D/0139.cpp.

---
+++ ABC/D/0139.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+void Main()
+{
+  ll n; cin >> n;
+  ll maxnum = 0;
+  for(int i = 2; i <= n; i++) {
+  	maxnum += i-1;
+  }
+  cout << maxnum << endl;
+}
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(20);
+  Main();
+  return 0;
+}
