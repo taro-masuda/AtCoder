80fc855b1fd7fd9748d0b377b981a50a09610fd0
Taro Masuda <taro.massu@gmail.com>
2020-07-22 15:52:18 +0900

    Added careful_selection_for_cyan/072.cpp.

---
+++ careful_selection_for_cyan/072.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+long long COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+void Main()
+{
+  COMinit();
+  ll w, h; cin >> w >> h;
+  cout << COM(w+h-2, min(w-1,h-1)) << endl;
+}
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cout << fixed << setprecision(20);
+  Main();
+  return 0;
+}
